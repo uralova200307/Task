4ace985c919bc5f352a3d5ae65c00837251d81e4
zarina <zarinauralova0307@gmail.com>
2023-03-30 22:00:39 +0500

    triple the same numbers

--- Task/Program.cs
+++ Task/Program.cs
@@ -4,7 +4,33 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int FirstNumber;
+            int SecondNumber;
+
+            // Displays the first number 
+            Console.Write(" Enter first number: ");
+            string Input = Console.ReadLine();
+            FirstNumber = Convert.ToInt32(Input);
+
+            //Displays the second number
+            Console.Write(" Enter second number: ");
+            string Output = Console.ReadLine();
+            SecondNumber = Convert.ToInt32(Output);
+
+            //A program to calculate the sum of two given integer values.
+            //If both values are the same, triple their sum.
+            if (FirstNumber == SecondNumber)
+            {
+                Console.WriteLine($" Result: {(FirstNumber + SecondNumber) * 3} ");
+            }
+            else
+            {
+                Console.WriteLine($" Result: {FirstNumber + SecondNumber}");
+
+            }
+
+            Main(args);
+
         }
     }
 }
