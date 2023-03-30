430872c04953dc4ec500daeb8b72c43d0178c840
zarina <zarinauralova0307@gmail.com>
2023-03-30 17:05:58 +0500

    binary number system

--- Task/Program.cs
+++ Task/Program.cs
@@ -4,7 +4,22 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int number;
+            string count = "";
+
+            Console.Write(" Enter number: ");
+            number = Convert.ToInt32(Console.ReadLine());
+
+            while (number >= 1)
+            {
+                count = (number % 2) + count;
+                number = number / 2;
+            }
+
+            Console.WriteLine(count);
+            Console.WriteLine();
+
+            Main(args);
         }
     }
 }
