a37c9d20cd066d8a9cb4110a6813c5ea38e316ba
zarina <zarinauralova0307@gmail.com>
2023-03-30 21:17:08 +0500

    multiplication table

--- Task/Program.cs
+++ Task/Program.cs
@@ -4,7 +4,14 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            for (int i = 1; i <= 10; i++)
+            {
+                for (int j = 1; j <= 10; j++)
+                {
+                    Console.WriteLine($"{i} * {j} = {j * i}");
+                }
+                Console.WriteLine();
+            }
         }
     }
 }
