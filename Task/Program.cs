1b6a913f3a859cfe194f2046a367ca0cebd14782
zarina <zarinauralova0307@gmail.com>
2023-03-30 21:42:16 +0500

    perfect number

--- Task/Program.cs
+++ Task/Program.cs
@@ -4,7 +4,38 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int number1, number2;
+
+            Console.Write(" Enter number: ");
+            number1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write(" Enter number: ");
+            number2 = Convert.ToInt32(Console.ReadLine());
+
+            for (int j = number1; j < number2; j++)
+            {
+
+                int value = 0;
+
+                for (int i = 1; i < j; i++)
+                {
+                    if (j % i == 0)
+                    {
+                        value += i;
+                    }
+
+                }
+
+                if (value == j)
+                {
+                    Console.WriteLine($" {j} - is a perfect number.");
+                } 
+                else
+                {
+                    Console.WriteLine($"{j} - is not a perfect number.");
+                }    
+
+            }
         }
     }
 }
