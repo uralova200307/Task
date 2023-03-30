01f390da0e2e412e95c909f21e350d58151e4ef9
zarina <zarinauralova0307@gmail.com>
2023-03-30 21:46:27 +0500

    prime number

--- Task/Program.cs
+++ Task/Program.cs
@@ -4,7 +4,29 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int number;
+
+            Console.Write(" Enter number: ");
+            number = Convert.ToInt32(Console.ReadLine());
+
+            int cout = 0;
+
+            for (int i = 1; i < number; i++)
+            {
+                if (number % i == 0)
+                {
+                    cout++;
+                }
+            }
+
+            if (cout <= 2)
+            {
+                Console.WriteLine($"{number} - a prime number");
+            }
+            else
+            {
+                Console.WriteLine($" {number} - not a prime number");
+            }
         }
     }
 }
