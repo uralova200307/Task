9d30c1359a2bb6d0fa2b5f0142f62362de6d0938
zarina <zarinauralova0307@gmail.com>
2023-03-31 09:24:35 +0500

    Max and Min number

--- Task/Program.cs
+++ Task/Program.cs
@@ -1,10 +1,35 @@
-﻿namespace Task
+﻿using System.ComponentModel.Design;
+
+namespace Task
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int firstnumber, secondnumber, thirdnumber, fourthnumber, max, min;
+
+
+            Console.Write(" Enter first number: ");
+            firstnumber = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write(" Enter second number: ");
+            secondnumber = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write(" Enter third number: ");
+            thirdnumber = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write(" Enter fourth number: ");
+            fourthnumber = Convert.ToInt32(Console.ReadLine());
+
+
+            max = Math.Max(fourthnumber,Math.Max(thirdnumber,Math.Max(firstnumber, secondnumber)));
+            min = Math.Min(fourthnumber,Math.Min(thirdnumber,Math.Min(firstnumber, secondnumber)));
+
+
+            Console.WriteLine($" Min number: {max}");
+            Console.WriteLine($" Min number: {min}");
+
+            Main(args);
         }
     }
 }
