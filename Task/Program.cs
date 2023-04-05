b8d5b2c6c235071391e18b3fc4a71ff0d88384ad
zarina <zarinauralova0307@gmail.com>
2023-04-05 08:59:25 +0500

    Find elements in an array that are divisible by k

--- Task/Program.cs
+++ Task/Program.cs
@@ -6,7 +6,27 @@ namespace Task
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(" Hello, worlid!");
+            Random random = new Random();
+
+            Console.Write("Enter the length: ");
+            int row = int.Parse(Console.ReadLine());
+            Console.Write("Enter the K: ");
+            int k = int.Parse(Console.ReadLine());
+            int[] array1 = new int[row];
+            for (int i = 0; i < array1.Length; i++)
+            {
+                array1[i] = random.Next(1, 20);
+                Console.Write($" {array1[i]}");
+
+            }
+            Console.WriteLine();
+            for (int i = 0; i < array1.Length; i++)
+            {
+                if (array1[i] % k == 0)
+                {
+                    Console.Write($" {array1[i]}");
+                }
+            }
         }
     }
 }
