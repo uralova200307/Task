d719c79bde7d6b616d57cd3e23c7515ad6361134
zarina <zarinauralova0307@gmail.com>
2023-04-05 08:54:40 +0500

    odd number in the array

--- Task/Program.cs
+++ Task/Program.cs
@@ -6,7 +6,25 @@ namespace Task
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(" Hello, worlid!");
+            Random random = new Random();
+
+            Console.Write("Enter the length: ");
+            int row = int.Parse(Console.ReadLine());
+            int[] array1 = new int[row];
+            for (int i = 0; i < array1.Length; i++)
+            {
+                array1[i] = random.Next(1, 10);
+                Console.Write($" {array1[i]}");
+
+            }
+            Console.WriteLine();
+            for (int i = 0; i < array1.Length; i++)
+            {
+                if (array1[i] % 2 != 0)
+                {
+                    Console.Write($" {array1[i]}");
+                }
+            }
         }
     }
 }
